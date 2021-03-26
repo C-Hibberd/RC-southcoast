b9c4f3db2398fa19ba1423ced990db9a47d7dcf2
C-Hibberd <55687047+C-Hibberd@users.noreply.github.com>
2021-03-26 13:40:54 +0000

    1st main submit

---
+++ resources/index.js
@@ -0,0 +1,11 @@
+import { Container } from 'container.js'
+
+window.mainContainer = new Container ({
+    panelID: 'record-panel',
+    hrefClass: 'record-panel-button',
+})
+
+window.scoreBoardTop = new Container ({
+    panelID: 'scoreBoard-panel',
+    hrefClass: 'scoreBoard-href'
+})
