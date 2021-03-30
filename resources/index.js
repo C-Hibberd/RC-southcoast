9b169cf09182d5de1ab6c4a0fb0af9e2121dc7b9
C-Hibberd <55687047+C-Hibberd@users.noreply.github.com>
2021-03-30 09:43:25 +0100

    yuh

---
+++ resources/index.js
@@ -0,0 +1,23 @@
+var i = 0;
+function add12Points() {
+  i += 12;
+  document.getElementById("inc").value = i;
+}
+
+var i = 0;
+function remove12Points() {
+  i -= 12;
+  document.getElementById("inc").value = i;
+}
+
+var i = 0;
+function add6Points() {
+  i += 6;
+  document.getElementById("inc").value = i;
+}
+
+var i = 0;
+function remove6Points() {
+  i -= 6;
+  document.getElementById("inc").value = i;
+}
