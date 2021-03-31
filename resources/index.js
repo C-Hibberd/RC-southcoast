6307b63c41a8505df39a3a1bdf08ed8fcc629c28
C-Hibberd <55687047+C-Hibberd@users.noreply.github.com>
2021-03-31 19:31:41 +0100

    mvp (kinda)

--- resources/index.js
+++ resources/index.js
@@ -1,23 +1,39 @@
+
 var i = 0;
+
 function add12Points() {
   i += 12;
   document.getElementById("inc").value = i;
 }
 
-var i = 0;
+
 function remove12Points() {
   i -= 12;
   document.getElementById("inc").value = i;
 }
 
-var i = 0;
+
 function add6Points() {
   i += 6;
   document.getElementById("inc").value = i;
 }
 
-var i = 0;
+
 function remove6Points() {
   i -= 6;
   document.getElementById("inc").value = i;
 }
+
+
+function classValueAdd() {
+  i += +document.getElementById("classinput").value;
+  document.getElementById("yuh").value = i;
+}
+
+
+function classValueRemove() {
+  i -= +document.getElementById("classinput").value;
+  document.getElementById("yuh").value = i;
+}
+
+
